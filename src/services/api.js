1affc05cb3fb43245fa52f3c122cb533441c10f0
Tobby Grey <olasunkonmitobiloba79@yahoo.com>
2026-06-17 10:46:13 +0100

    Fix API URL to production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://tobby-delivery-backend.onrender.com/api"
+  baseURL: "https://tobby-delivery-backend.onrender.com/api",
+  timeout: 30000,
 });
 
 API.interceptors.request.use((config) => {
@@ -9,5 +10,17 @@ API.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+API.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("agentToken");
+      localStorage.removeItem("agentUser");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default API;
